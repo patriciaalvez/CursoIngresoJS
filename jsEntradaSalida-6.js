7fe564191f17331b6c286f8af00acbf10053ba68
patriciaalvez <36265132+patriciaalvez@users.noreply.github.com>
2018-02-09 11:27:03 -0300

    Ejercicios del 1 al 6
    variables, prompt, alert, etc

---
+++ jsEntradaSalida-6.js
@@ -0,0 +1,27 @@
+{
+var numeroUNo;
+    var numeroDos;
+    var resultado; 
+
+    numeroUno=document.getElementById('numeroUno').value;
+    numeroDos=document.getElementById('numeroDos').value;
+    numeroUno=parseInt(numeroUno);
+    numeroDos=parseInt(numeroDos);
+    
+   // numeroUno=700;
+    //numeroDos=50;
+    
+    resultado=numeroUno+numeroDos;
+    alert(resultado);
+
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt) y Sumarlos.
+Mostar el resulto por medio de "ALERT"*/
+function sumar()
+
+
+
+
+}
+
+
